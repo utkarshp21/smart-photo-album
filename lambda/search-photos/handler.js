88a9858da88c17b9eeab579cdeb76e9e97d2730f
Utkarsh Prakash <monsieurutkarsh@gmail.com>
2019-04-28 22:55:12 -0400

    removed placehoders and commented code

--- lambda/search-photos/handler.js
+++ lambda/search-photos/handler.js
@@ -4,19 +4,17 @@ const lexService = require('./lexService');
 
 module.exports.search = async (event, context) => {
   
-  // console.log("Received query - " + event.queryStringParameters.q);
-  let searchText = "Show me dogs";//event.queryStringParameters.q;
+  let searchText = event["searchQuery"];
   let keywords = await lexService.extractKeywords(searchText);
-  let paths = await elasticSearchService.getPhotoPaths(keywords);
+
+  let imagePaths = await elasticSearchService.getPhotoPaths(keywords);
   
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      paths: paths,
-      input: event,
-    }),
+    body: {
+      imagePaths: imagePaths,
+      userQuery: event["searchQuery"],
+    },
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
